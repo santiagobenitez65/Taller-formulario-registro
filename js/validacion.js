c50e3ea650eb900cc87a2b3cff6f9771ecc58701
santiagobenitez65 <santiagobenitez652005@gmail.com>
2025-08-14 19:49:09 -0300

    Agregado el eventListener

--- js/validacion.js
+++ js/validacion.js
@@ -6,35 +6,44 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-const nombre = document.getElementById("nombre").value.trim();
-const apellido = document.getElementById("apellido").value.trim();
-const email = document.getElementById("email").value.trim();
-const password1 = document.getElementById("password1").value;
-const password2 = document.getElementById("password2").value;
-const terminos = document.getElementById("terminos").value;
+const boton = document.getElementById("regBtn")
 
-function camposVacios() {
-    return(!nombre || !apellido || !email || !password1 || !password2);
-}
-
-function terminosNoValidados () {
-    return !terminos.checked;
-}
-
-function contrasenasDiferentes() {
-    return password1 !== password2;
-}
-function validarPassword(){
-    const password1 = document.getElementById("password1").value;
-    
-    document.getElementById("alert-success").classList.remove("show");
-    document.getElementById("alert-danger").classList.remove("show");
-
-
-    if (password1.length < 6) {
-        showAlertError();
+function camposLlenos(nombre, apellido, email, password1, password2) {
+    if(nombre && apellido && email && password1 && password2) {
+        return true
     } else {
-        showAlertSuccess();
+        return false
     }
 }
 
+function terminosValidados (terminos) {
+    return terminos.checked;
+}
+
+function contrasenasIguales(password1, password2) {
+    return password1 === password2;
+}
+
+function validarPassword(password1){
+   return password1.length >= 6;
+}
+
+boton.addEventListener("click", function() {
+    
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const terminos = document.getElementById("terminos");
+    
+    console.log("Campos llenos: " + camposLlenos(nombre, apellido, email, password1, password2));
+    console.log("Contraseñas iguales: " + contrasenasIguales(password1, password2));
+    console.log("Terminos validados: " + terminosValidados(terminos));
+
+    if(camposLlenos(nombre, apellido, email, password1, password2) && validarPassword(password1) && contrasenasIguales(password1, password2) && terminosValidados(terminos)) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+})
