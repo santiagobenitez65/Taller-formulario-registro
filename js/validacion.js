ddc1189f14abf839297c5b69d88fa6ec6e450b1f
Joaquin30103 <joaquinfonsalia04@gmail.com>
2025-08-14 18:46:26 -0300

    ContraseñaDiferente
    .

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+
+    if (password1 !== password2) {
+        showAlertError();
+        return;
+    }
+    
+
+})
